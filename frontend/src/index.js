b2ac3b803bc491e2e0decdda0510d7cddbae5026
EXCOff <santyexcofier@gmail.com>
2024-06-14 19:27:01 -0300

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+function AppWithCallbackAfterRender() {
+  useEffect(() => {
+    console.log('El componente se ha renderizado');
+  }, []);
 
-root.render(<App />);
+  return <App />;
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+ReactDOM.render(<AppWithCallbackAfterRender />, document.getElementById('root'));
