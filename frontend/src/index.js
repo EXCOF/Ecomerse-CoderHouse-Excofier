965d194f53bb2e0485bd5eceb67f0a3a9db141a6
EXCOff <santyexcofier@gmail.com>
2024-06-14 18:23:02 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Asegúrate de importar ReactDOM
-import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-const root = createRoot(document.getElementById("root")); // No es necesario usar ReactDOM.createRoot
+
 root.render(<App />);
