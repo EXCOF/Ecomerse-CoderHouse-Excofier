16279808ef07e6a28a9e9609ecd498f254541882
EXCOff <santyexcofier@gmail.com>
2024-06-14 17:39:57 -0300

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react'; // Importa React
-import ReactDOM from 'react-dom'; // Importa ReactDOM
-import App from './app'; // Importa el componente App desde app.js
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap CSS
-import './style.css'; // Importa los estilos CSS personalizados
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 import { createRoot } from 'react-dom/client';
-
 
 // Crear el root
 const container = document.getElementById('app');
